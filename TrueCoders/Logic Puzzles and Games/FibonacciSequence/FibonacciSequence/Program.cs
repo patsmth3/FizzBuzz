d02fbbfa6b3f58994bccccafce109481e1fef3db
Pat Smith <patsmith@gmail.com>
2021-05-27 11:16:39 -0400

    Fibonacci sequence solved using a loop

---
+++ TrueCoders/Logic Puzzles and Games/FibonacciSequence/FibonacciSequence/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci loop solution");
+
+            var number1 = 0 ;
+            var number2 = 1;
+
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+            
+            for (int i = 0; i <= 15; i++)
+            {
+                var next = number1 + number2;
+                number1 = number2;
+                number2 = next;
+
+                Console.WriteLine(next);
+            }
+        }
+    }
+}
